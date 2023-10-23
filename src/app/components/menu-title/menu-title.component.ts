1aca368f578ef629b27a005b3f3adaf71b8f68eb
MkDuarte <mkdu185@gmail.com>
2023-10-23 12:57:15 -0300

    feat: Add menu-title component

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrls: ['./menu-title.component.css']
+  styleUrls: [
+    './menu-title.component.css',
+    './menu-title.responsive.component.css'
+
+]
 })
-export class MenuTitleComponent {
+export class MenuTitleComponent implements OnInit {
+  constructor() {}
+    ngOnInit(): void {
+      
+    }
+  
 
 }
