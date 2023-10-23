f7ae2fdb0b6689be24ab2a2c7fdca0760e211a3c
MkDuarte <mkdu185@gmail.com>
2023-10-23 13:04:47 -0300

    feat: create dataFake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novo filme da Marvel em breve",
+    "description": "marvel anuncia novo filme, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "2",
+    "title": "Nova série anunciada",
+    "description": "nova série começa em dezembro",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
